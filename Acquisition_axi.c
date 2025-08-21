582a4083a6906af8182c83408d0bf0263c6d7b49
page-icube <33966313+klaw83@users.noreply.github.com>
2025-08-21 10:54:14 +0200

    Version 1 (comments)

--- Acquisition_axi.c
+++ Acquisition_axi.c
@@ -1,5 +1,6 @@
 /* Red Pitaya C API example Acquiring a signal from a buffer
  * This application acquires a signal on a specific channel */
+// VERSION 1 - Uiliser Lecture_fichier.ipynb
 
 #include <stdio.h>
 #include <stdlib.h>
